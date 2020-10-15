5eb98c77c19a3809899c34b25fcaa104420bdea3
paluch05 <72819937+paluch05@users.noreply.github.com>
2020-10-15 22:09:52 +0200

    Add files via upload

---
+++ Task3.java
@@ -0,0 +1,9 @@
+package pl.orzechsoft.course.homework3;
+
+public class Task3 {
+    public static void main(String[] args) {
+        String s1 = "123456";
+        String s2 = "234567";
+        System.out.println(s1.length() == s2.length());
+    }
+}
