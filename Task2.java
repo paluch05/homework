ce7c5f104828cd4f7e6dbd86d509bab1e513f24a
paluch05 <72819937+paluch05@users.noreply.github.com>
2020-10-15 19:00:30 +0200

    Add files via upload

---
+++ Task2.java
@@ -0,0 +1,15 @@
+package pl.orzechsoft.course.Task2;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int a = 55;
+        int b = 10;
+        boolean isAdding = true;
+
+        if (isAdding) {
+            System.out.println(a + " plus " + b + " is " + (a + b));
+        } else {
+            System.out.println(a + " minus " + b + " is " + (a - b));
+        }
+    }
+}
