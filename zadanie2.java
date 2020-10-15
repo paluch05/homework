347942a188b92b04bb5fcbdb0f8ad557b8754309
paluch05 <72819937+paluch05@users.noreply.github.com>
2020-10-15 16:14:23 +0200

    Add files via upload

--- zadanie2.java
+++ zadanie2.java
@@ -2,15 +2,14 @@ package pl.orzechsoft.course.zadanie2;
 
 public class zadanie2 {
     public static void main(String[] args) {
-
         int a = 55;
         int b = 10;
         boolean isAdding = true;
 
-        if (isAdding == true){
-            System.out.println(""+ a +" plus "+ b +" is "+ (a + b) +"");
+        if (isAdding == true) {
+            System.out.println("" + a + " plus " + b + " is " + (a + b) + "");
         } else {
-            System.out.println(""+ a +" minus "+ b +" is "+ (a - b) +"");
+            System.out.println("" + a + " minus " + b + " is " + (a - b) + "");
         }
     }
 }
