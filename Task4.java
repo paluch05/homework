5521b9767228ed4b95a4c46aa3e62f0b8bdd9720
paluch05 <72819937+paluch05@users.noreply.github.com>
2020-10-19 22:32:18 +0200

    Add files via upload

---
+++ Task4.java
@@ -0,0 +1,19 @@
+package pl.orzechsoft.homework4.task4;
+
+public class Task4 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 151; i++)
+            if (i % 3 != 0 ) {
+                System.out.print(i + " ");
+                if (i % 4 == 0) {
+                    System.out.print("Zio");
+                    if (i % 5 == 0) {
+                        System.out.print("mek");
+                    }
+                } else if (( i % 5 == 0 ) && ( i % 4 != 0 )) {
+                    System.out.print("mek");
+                }
+                System.out.println();
+            }
+        }
+    }
