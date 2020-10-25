cbe53283869c9632eaf86ed724f15b9494e46901
paluch05 <annapaluch05@gmail.com>
2020-10-25 12:25:10 +0100

    changed HelloWorld (Task1) to use methods

--- src/main/java/pl/orzechsoft/paluch/tasks/Task2.java
+++ src/main/java/pl/orzechsoft/paluch/tasks/Task2.java
@@ -9,23 +9,23 @@ public class Task2 {
         printResult(a, b, isAdding);
     }
 
-    private static void printResult(int a, int b, boolean isAdding) {
-        if (isAdding) {
-            System.out.println(a + " plus " + b + " is " + (a + b));
-        } else {
-            System.out.println(a + " minus " + b + " is " + (a - b));
-        }
-    }
-
-    private static boolean isAdding() {
-        return true;
+    private static int getA() {
+        return 55;
     }
 
     private static int getB() {
         return 10;
     }
 
-    private static int getA() {
-        return 55;
+    private static boolean isAdding() {
+        return true;
+    }
+
+    private static void printResult(int a, int b, boolean isAdding) {
+        if (isAdding) {
+            System.out.println(a + " plus " + b + " is " + (a + b));
+        } else {
+            System.out.println(a + " minus " + b + " is " + (a - b));
+        }
     }
 }
--- src/main/java/pl/orzechsoft/paluch/tasks/Task5.java
+++ src/main/java/pl/orzechsoft/paluch/tasks/Task5.java
@@ -2,10 +2,23 @@ package pl.orzechsoft.paluch.tasks;
 
 public class Task5 {
     public static void main(String[] args) {
-        String s1 = "abcdef";
-        String s2 = "kbhdjf";
+        String s1 = getString();
+        String s2 = getSecondString();
         boolean areOddEqual = true;
         boolean areEvenEqual = true;
+        halfEqual(s1, s2, areOddEqual, areEvenEqual);
+    }
+
+    private static String getString() {
+        return "abcdef";
+    }
+
+    private static String getSecondString() {
+        return "kbhdjf";
+    }
+
+
+    private static void halfEqual(String s1, String s2, boolean areOddEqual, boolean areEvenEqual) {
         for (int i = 1; i <= s1.length() && i < s2.length(); i = i + 2) {
             areEvenEqual = s1.charAt(i - 1) == s2.charAt(i - 1) && areEvenEqual;
             areOddEqual = s1.charAt(i) == s2.charAt(i) && areOddEqual;
