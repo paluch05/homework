102bbcf01fe4cf0af4da9cbc87a826cdb16522b3
paluch05 <annapaluch05@gmail.com>
2020-10-25 16:28:19 +0100

    changed Task2 to use methods isAdding and subtract

--- src/main/java/pl/orzechsoft/paluch/tasks/Task2.java
+++ src/main/java/pl/orzechsoft/paluch/tasks/Task2.java
@@ -5,8 +5,10 @@ public class Task2 {
         int a = getA();
         int b = getB();
         boolean isAdding = isAdding();
+        boolean subtract = subtract();
 
-        printResult(a, b, isAdding);
+        isAdding(a, b, isAdding, " plus ", a + b);
+        subtract(a, b, subtract, " minus ", a - b);
     }
 
     private static int getA() {
@@ -21,11 +23,19 @@ public class Task2 {
         return true;
     }
 
-    private static void printResult(int a, int b, boolean isAdding) {
+    private static boolean subtract() {
+        return true;
+    }
+
+    private static void isAdding(int a, int b, boolean isAdding, String s, int i) {
         if (isAdding) {
-            System.out.println(a + " plus " + b + " is " + (a + b));
-        } else {
-            System.out.println(a + " minus " + b + " is " + (a - b));
+            System.out.println(a + s + b + " is " + (i));
+        }
+    }
+
+    private static void subtract(int a, int b, boolean subtract, String s, int i) {
+        if (subtract) {
+            System.out.println(a + s + b + " is " + (i));
         }
     }
 }
