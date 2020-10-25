6e0bc757b902861d1d63b061c080e008d9053299
paluch05 <annapaluch05@gmail.com>
2020-10-25 12:12:05 +0100

    changed Task2 to use methods

--- src/main/java/pl/orzechsoft/paluch/tasks/Task2.java
+++ src/main/java/pl/orzechsoft/paluch/tasks/Task2.java
@@ -2,14 +2,30 @@ package pl.orzechsoft.paluch.tasks;
 
 public class Task2 {
     public static void main(String[] args) {
-        int a = 55;
-        int b = 10;
-        boolean isAdding = true;
+        int a = getA();
+        int b = getB();
+        boolean isAdding = isAdding();
 
+        printResult(a, b, isAdding);
+    }
+
+    private static void printResult(int a, int b, boolean isAdding) {
         if (isAdding) {
             System.out.println(a + " plus " + b + " is " + (a + b));
         } else {
             System.out.println(a + " minus " + b + " is " + (a - b));
         }
+    }
+
+    private static boolean isAdding() {
+        return true;
+    }
+
+    private static int getB() {
+        return 10;
+    }
+
+    private static int getA() {
+        return 55;
     }
 }
