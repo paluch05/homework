0cf98c47646f0cd4be14c0d08fead4062defc205
paluch05 <annapaluch05@gmail.com>
2020-10-26 20:02:56 +0100

    corrected Task7-6

--- src/main/java/pl/orzechsoft/paluch/tasks/Task6.java
+++ src/main/java/pl/orzechsoft/paluch/tasks/Task6.java
@@ -3,16 +3,15 @@ package pl.orzechsoft.paluch.tasks;
 public class Task6 {
     public static void main(String args[]) {
         int a = 0, b = 1, c;
-        int whichFibonacciNumber = 5;
-        getPrintln(whichFibonacciNumber);
+        int getFibonacciNumber;
+        printResult(getWhichFibonacciNumber());
     }
 
-    public static int fib(int whichFibonacciNumber) {
-        int c;
-        return calc(whichFibonacciNumber, 0, 1);
+    private static int getWhichFibonacciNumber() {
+        return 5;
     }
 
-    private static int calc(int whichFibonacciNumber, int a, int b) {
+    private static int getFibonacciNumber(int whichFibonacciNumber, int a, int b) {
         int c;
         if (whichFibonacciNumber == 0) {
             return a;
@@ -25,7 +24,7 @@ public class Task6 {
         return b;
     }
 
-    private static void getPrintln(int whichFibonacciNumber) {
-        System.out.println(fib(whichFibonacciNumber));
+    private static void printResult(int whichFibonacciNumber) {
+        System.out.println(getWhichFibonacciNumber());
     }
 }
