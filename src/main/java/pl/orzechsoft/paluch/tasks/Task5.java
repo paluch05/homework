f5490c614c7a9d46bad6fb7cf030d8452e49f4c5
paluch05 <annapaluch05@gmail.com>
2020-10-26 14:07:50 +0100

    Corrected Task 5

--- src/main/java/pl/orzechsoft/paluch/tasks/Task5.java
+++ src/main/java/pl/orzechsoft/paluch/tasks/Task5.java
@@ -4,9 +4,7 @@ public class Task5 {
     public static void main(String[] args) {
         String s1 = getString();
         String s2 = getSecondString();
-        boolean areOddEqual = isAreOddEqual();
-        boolean areEvenEqual = isAreEvenEqual();
-        halfEqual(s1, s2, areOddEqual, areEvenEqual);
+        halfEqual(s1, s2);
     }
 
     private static String getString() {
@@ -17,15 +15,9 @@ public class Task5 {
         return "kbhdjf";
     }
 
-    private static boolean isAreOddEqual() {
-        return true;
-    }
-
-    private static boolean isAreEvenEqual() {
-        return true;
-    }
-
-    private static void halfEqual(String s1, String s2, boolean areOddEqual, boolean areEvenEqual) {
+    private static void halfEqual(String s1, String s2) {
+        boolean areOddEqual = true;
+        boolean areEvenEqual = true;
         for (int i = 1; i <= s1.length() && i < s2.length(); i = i + 2) {
             areEvenEqual = s1.charAt(i - 1) == s2.charAt(i - 1) && areEvenEqual;
             areOddEqual = s1.charAt(i) == s2.charAt(i) && areOddEqual;
