0ef8ed893ea7b97b41e40f081590a9d08fd70ab2
paluch05 <annapaluch05@gmail.com>
2020-10-26 19:31:04 +0100

    corrected Task7-5

--- src/main/java/pl/orzechsoft/paluch/tasks/Task5.java
+++ src/main/java/pl/orzechsoft/paluch/tasks/Task5.java
@@ -2,9 +2,8 @@ package pl.orzechsoft.paluch.tasks;
 
 public class Task5 {
     public static void main(String[] args) {
-        String s1 = getString();
-        String s2 = getSecondString();
-        halfEqual(s1, s2);
+        halfEqual(getString(), getSecondString(), true, true);
+        print(true || true);
     }
 
     private static String getString() {
@@ -15,14 +14,16 @@ public class Task5 {
         return "kbhdjf";
     }
 
-    private static void halfEqual(String s1, String s2) {
-        boolean areOddEqual = true;
-        boolean areEvenEqual = true;
+    private static boolean halfEqual(String s1, String s2, boolean areEvenEqual, boolean areOddEqual) {
         for (int i = 1; i <= s1.length() && i < s2.length(); i = i + 2) {
             areEvenEqual = s1.charAt(i - 1) == s2.charAt(i - 1) && areEvenEqual;
             areOddEqual = s1.charAt(i) == s2.charAt(i) && areOddEqual;
         }
-        if (areEvenEqual || areOddEqual) {
+        return areEvenEqual || areOddEqual;
+    }
+
+    private static void print(boolean result) {
+        if (result) {
             System.out.println("Half are equal!");
         }
     }
