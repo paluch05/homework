545d9f578d447f20e73c49f6114f692a6650512d
paluch05 <annapaluch05@gmail.com>
2020-11-03 22:35:24 +0100

    Corrected Task11

--- src/main/java/pl/orzechsoft/paluch/tasks/Task11/Task11.java
+++ src/main/java/pl/orzechsoft/paluch/tasks/Task11/Task11.java
@@ -1,32 +1,30 @@
 package pl.orzechsoft.paluch.tasks.Task11;
 
 import java.util.Calendar;
-import java.util.List;
 import java.util.Scanner;
+
+import static pl.orzechsoft.paluch.tasks.Task11.BlackList.BLACK_LIST;
 
 public class Task11 {
 
     private static String pesel;
-    private static final List<String> BLACK_LIST = List.of("Zakazany przedmiot 1", "Glock", "Marijuana");
-    private static final int AGE = 18;
-
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your PESEL here: ");
         pesel = sc.nextLine();
-        String check = mature(pesel, 18);
-        System.out.println(((checkPESEL()) ? check : "You tried to trick me you cheeky bastard!"));
+        String check = mature(pesel, Integer.parseInt(args[0]));
+        System.out.println(checkPESEL() ? check : "You tried to trick me you cheeky bastard!");
         String sell = sc.nextLine();
         System.out.println(checkList(sell));
     }
 
     public static boolean checkPESEL() {
-        int[] weigth = {1, 3, 7, 9, 1, 3, 7, 9, 1, 3};
+        int[] weigths = {1, 3, 7, 9, 1, 3, 7, 9, 1, 3};
         if (pesel.length() != 11) return false;
         int sum = 0;
         for (int i = 0; i < 10; i++)
-            sum += Integer.parseInt(pesel.substring(i, i + 1)) * weigth[i];
+            sum += Integer.parseInt(pesel.substring(i, i + 1)) * weigths[i];
         int control = Integer.parseInt(pesel.substring(10, 11));
         sum %= 10;
         sum = 10 - sum;
@@ -34,24 +32,23 @@ public class Task11 {
         return sum == control;
     }
 
-    private static String mature(String pesel, int AGE) {
-        AGE = 18;
+    private static String mature(String pesel, int age) {
         int range = Integer.parseInt(pesel.substring(2, 3));
         int year = Integer.parseInt(pesel.substring(0, 2));
         Calendar cal = Calendar.getInstance();
         int currentYear = Integer.parseInt(String.valueOf(cal.get(Calendar.YEAR)).substring(2, 4));
-        String matureOrNot = "";
+        String message = "";
         if (range != 8 && range != 0 && range != 1) {
-            if ((range == 2 || range == 4 || range == 6) && (currentYear - year) >= AGE) {
-                matureOrNot = "What do you want to sell?";
+            if ((range == 2 || range == 4 || range == 6) && (currentYear - year) >= age) {
+                message = "What do you want to sell?";
             } else {
-                int difference = AGE - (currentYear - year);
-                matureOrNot = "Sorry, come back in " + difference + " years!";
+                int difference = age - (currentYear - year);
+                message = "Sorry, come back in " + difference + " years!";
             }
         } else {
-            matureOrNot = "What do you want to sell?";
+            message = "What do you want to sell?";
         }
-        return matureOrNot;
+        return message;
     }
 
     public static String checkList(String sell) {
