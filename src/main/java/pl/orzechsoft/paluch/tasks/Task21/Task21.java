436880b94191b9d0f58ce2edf3a2299a86d301b0
paluch05 <annapaluch05@gmail.com>
2020-11-21 20:39:12 +0100

    Task21

---
+++ src/main/java/pl/orzechsoft/paluch/tasks/Task21/Task21.java
@@ -0,0 +1,18 @@
+package pl.orzechsoft.paluch.tasks.Task21;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Task21 {
+
+    public static void main(String[] args) {
+        List<String> names = List.of("Marcin", "Adam", "Agata", "Anna", "Mateusz", "Wojtek", "Kasia", "Tomek", "Kamil",
+                        "Maria", "Arkadiusz", "Przemysław", "Horacy", "Zofia", "Genowefa", "Serafin");
+        Random random = new Random();
+        List<Person> persons = names.stream().map(name -> new Person(name, random.nextInt(23 - 15) + 15)).collect(Collectors.toList());
+        Set<Person> adults = persons.stream().filter(person -> (person.getAge() >= 18)).collect(Collectors.toSet());
+        adults.forEach(person -> {
+                    System.out.println(person.getName() + " is " + person.getAge() + " years old.");
+                });
+    }
+}
